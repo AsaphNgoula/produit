502fae4e0d0cf2a3f3045817b9a47cf5f710b7d1
asaph <asaphngoula237@gmail.com>
2026-03-07 09:21:43 +0100

    create courses_class

---
+++ formation/courses/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+
+app_name='courses'
+urlpatterns = [
+    # path('',ArticleListview.as_view(),name='article-list'),
+    # path('detail/<int:pk>', ArticleDetailview.as_view(), name='article-detail'),
+    # path('update/<int:pk>',ArticleUpdateView.as_view(), name='article-update'),
+    # path('create', ArticleCreateView.as_view(),name='article_create'),
+    # path('delete/<int:pk>',ArticleDeleteView.as_view(), name='article-delete'),
+
+]
---
+++ formation/courses/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views import Views
+from .forms import CourseForm
+
+# Create your views here.
+class List_Views
