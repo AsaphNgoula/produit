ec3be8675913c121593d478f73a553bc73efe60a
asaph <asaphngoula237@gmail.com>
2026-03-07 22:05:11 +0100

    add class language and class Framework

--- formation/courses/models.py
+++ formation/courses/models.py
@@ -16,3 +16,19 @@ class Courses(models.Model):
     def get_absolute_url(self):
         return reverse("courses:course-detail", kwargs={"pk": self.pk})
     
+class language(models.Model):
+    name=models.CharField(max_length=20)
+
+
+class Framework(models.Model):
+    name=models.CharField(max_length=20)
+    language=models.ForeignKey(language, on_delete=models.CASCADE)
+
+
+"""
+CASCADE:lorsqu'on supprime le language la classe framework ne sert plus a rien
+PROTECT:le contraire lorqu'on supprime la classe language la classe framework fonctionne
+SET_NULL: lorsque je supprime la classe language, dans la classe framework je met NULL au niveau du langage
+SET_DEFAULT: on donne une valeur par defaut apres supression
+DO_NOTHING:lorsque je supprime je laisse la table comme elle est
+"""
